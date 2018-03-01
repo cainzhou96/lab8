a7f1c864f28d5667fba9f3f3fc4708fef75ee561
Cain <cainzhou96@gmail.com>
2018-02-28 22:08:55 -0800

    done

--- public/js/googleMaps.js
+++ public/js/googleMaps.js
@@ -1,3 +1,13 @@
 function initMap() {
-	// add your code here
+	var ucsd_ltlng = {lat:32.878855, lng:-117.235875}; 
+	var map = new google.maps.Map(document.getElementById('map'), {
+		center: ucsd_ltlng, 
+		zoom: 15
+	}); 
+
+	var marker = new google.maps.Marker({
+		position: ucsd_ltlng,  
+		map: map, 
+		title: "UCSD"
+	}); 
 }
